97763c47d1b59f071ab05a3e1ea8d71eff2e167e
DOOINSEO <ddll0837@gmail.com>
2024-09-25 13:37:47 +0900

    🎉 시작

---
+++ src/main/kotlin/kr/ac/kumoh/s20210395/w24w0301wcss/W24w0301wcssApplication.kt
@@ -0,0 +1,11 @@
+package kr.ac.kumoh.s20210395.w24w0301wcss
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class W24w0301wcssApplication
+
+fun main(args: Array<String>) {
+	runApplication<W24w0301wcssApplication>(*args)
+}
---
+++ src/test/kotlin/kr/ac/kumoh/s20210395/w24w0301wcss/W24w0301wcssApplicationTests.kt
@@ -0,0 +1,13 @@
+package kr.ac.kumoh.s20210395.w24w0301wcss
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class W24w0301wcssApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
